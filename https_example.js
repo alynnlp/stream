12ffed3343d7cde01499cf56517f1c0759c19514
Aileen Pang <aileenpang63@gmail.com>
2017-09-12 00:26:49 +0000

    reinitialized files

--- https_example.js
+++ https_example.js
@@ -1,4 +1,24 @@
 var https = require('https');
+//console.log('I did it!');
 
 
-console.log('I did it!');
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+// called by https when the request is made.
+var callback = function() {
+  console.log('In response handler callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
+
+//return node https_example.js
+// I'm about to make the request!
+// I've made the request!
+// In response handler callback!
