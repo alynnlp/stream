838f66406d5ae4bc35c4b9318d84eccd5726be95
Aileen Pang <aileenpang63@gmail.com>
2017-09-12 02:03:58 +0000

    final version of entire Monty Python script via an HTTP stream.

--- https_example.js
+++ https_example.js
@@ -1,22 +1,23 @@
 var https = require('https');
 //console.log('I did it!');
 
-var options = {
-  host: 'example.org',
-  path: '/'
-};
-
-//host a simple web server that
-//sends back an entire Monty Python script via an HTTP stream.
 // var options = {
-//   host: 'stream-large-file.herokuapp.com',
-//   path: '/give-me-stuff-now'
+//   host: 'example.org',
+//   path: '/'
 // };
+
+// host a simple web server that
+// sends back an entire Monty Python script via an HTTP stream.
+var options = {
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
+};
 
 // called by https when the request is made.
 // HOWEVER,we need to provide a response argument to our callback function
 // in order to read the response.
 
+//it will return us 'stream of incoming message' thats not what we want
 // var callback = function(response) {
 //   console.log('In response handler callback!');
 //   console.log('Response: ', response);
